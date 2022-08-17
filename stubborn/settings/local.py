e3fc78b72d9c6092b08c6cc7349ef30b1e9f75db
Sergei Nagaev <nagaev.sv@gmail.com>
2022-08-17 21:03:51 +0200

    Sonar warnings fixed.

--- stubborn/settings/local.py
+++ stubborn/settings/local.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = ['*']
 
 # APPS
 # http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
-INSTALLED_APPS: List[str] = ['whitenoise.runserver_nostatic', 'django_extensions'] + INSTALLED_APPS  # noqa F405
+INSTALLED_APPS: List[str] = ['django_extensions'] + INSTALLED_APPS  # noqa F405
 
 LOGGING = {
     "version": 1,
