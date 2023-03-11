382da6bdc4154b0bacd0fd73bc16f0f41e30ab76
Sergei Nagaev <nagaev.sv@gmail.com>
2023-03-11 16:33:50 +0100

    Codecov settings added

--- apps/tests/test_services.py
+++ apps/tests/test_services.py
@@ -3,11 +3,10 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from apps.services import get_same_enabled_resource_stub, proxy_request, turn_off_same_resource_stub
-from apps.tests.data import create_application, create_resource_stub, create_response_stub
+from apps.tests.data import create_application, create_resource_stub
 
 
 class TestProxy:
-    
     @patch('requests.request', return_value=None)
     @pytest.mark.parametrize('request_method', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
     def test_proxy_request_func(self, mocked_request_method, request_method):
@@ -29,22 +28,26 @@ class TestProxy:
             headers={'Content-Type': 'application/json'},
             data=b'{"Status":"OK"}',
         )
-        
+
     @pytest.mark.django_db
     def test_get_same_enabled_resource_stub_resource_exists(self):
         application = create_application()
         slug = 'testslug'
         tail = 'some/tail/here'
-        
+
         resource_disabled = create_resource_stub(
-            application=application, method='GET', slug=slug, tail=tail, is_enabled=False,
-            )
+            application=application,
+            method='GET',
+            slug=slug,
+            tail=tail,
+            is_enabled=False,
+        )
         resource_enabled = create_resource_stub(
             application=application, method='GET', slug=slug, tail=tail, is_enabled=True
-            )
-        
+        )
+
         same_stub = get_same_enabled_resource_stub(resource_disabled)
-        
+
         assert same_stub
         assert same_stub == resource_enabled
 
@@ -52,37 +55,39 @@ class TestProxy:
     def test_get_same_enabled_resource_stub_resource_doesnt_exist(self):
         application = create_application()
         resource_disabled = create_resource_stub(application=application, method='GET', is_enabled=False)
-        
+
         for _ in range(10):
             create_resource_stub(application=application, method='GET', is_enabled=True)
-        
-        same_stub = get_same_enabled_resource_stub(resource_disabled)
-        
-        assert not same_stub
 
+        same_stub = get_same_enabled_resource_stub(resource_disabled)
+
+        assert not same_stub
 
     @pytest.mark.django_db
     def test_turn_off_same_resource_stub_resource_exists(self):
         application = create_application()
         slug = 'testslug'
         tail = 'some/tail/here'
-        
+
         resource_disabled = create_resource_stub(
-            application=application, method='GET', slug=slug, tail=tail, is_enabled=False,
-            )
+            application=application,
+            method='GET',
+            slug=slug,
+            tail=tail,
+            is_enabled=False,
+        )
         resource_enabled = create_resource_stub(
             application=application, method='GET', slug=slug, tail=tail, is_enabled=True
-            )
-        
+        )
+
         turned_off_resource = turn_off_same_resource_stub(resource_stub=resource_disabled)
         assert turned_off_resource
-        
+
         resource_enabled.refresh_from_db()
         assert turned_off_resource == resource_enabled
 
-
     @pytest.mark.django_db
-    def test_turn_off_same_resource_stub_resource_exists(self):
+    def test_turn_off_same_resource_stub_resource_doesnt_exist(self):
         application = create_application()
         resource_disabled = create_resource_stub(application=application, method='GET', is_enabled=False)
         turned_off_resource = turn_off_same_resource_stub(resource_stub=resource_disabled)
