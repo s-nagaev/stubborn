e463b1fd9a7a5711188f82cfb723e3df20f454ac
ksenyachertenko <ksenyachertenko@gmail.com>
2024-04-21 16:30:28 +0400

    Add Team migration.

--- apps/models.py
+++ apps/models.py
@@ -87,10 +87,10 @@ class AbstractHTTPObject(models.Model):
         return jinja_template.render(fake=Faker(), random=random)
 
 
-class Member(User):
-    User._meta.get_field('email')._unique = True
-    displayed_name = models.CharField(max_length=100, verbose_name='Displayed name', null=True, blank=True)
-    teams = models.ManyToManyField('Team', verbose_name='Teams', related_name='members')
+# class Member(User):
+#     User._meta.get_field('email')._unique = True
+#     displayed_name = models.CharField(max_length=100, verbose_name='Displayed name', null=True, blank=True)
+#     teams = models.ManyToManyField('Team', verbose_name='Teams', related_name='members')
 
 
 class Application(BaseStubModel):
@@ -98,7 +98,7 @@ class Application(BaseStubModel):
     name = models.CharField(max_length=50, verbose_name='Name', null=False)
     slug = models.SlugField(verbose_name='Slug', allow_unicode=True, null=False, unique=False)
     owner = models.ForeignKey(
-        Member,
+        User,
         verbose_name='Application Owner',
         null=True,
         blank=True,
@@ -166,7 +166,7 @@ class ResponseStub(AbstractHTTPObject, BaseStubModel):
         related_name='responses',
     )
     creator = models.ForeignKey(
-        Member, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='responses'
+        User, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='responses'
     )
 
     class Meta:
@@ -201,7 +201,7 @@ class RequestStub(AbstractHTTPObject, BaseStubModel):
         related_name='requests',
     )
     creator = models.ForeignKey(
-        Member, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='request'
+        User, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='request'
     )
 
     class Meta:
@@ -250,7 +250,7 @@ class ResourceStub(BaseStubModel):
         blank=True,
     )
     creator = models.ForeignKey(
-        Member, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='resources'
+        User, verbose_name='Created by', null=True, blank=True, on_delete=models.SET_NULL, related_name='resources'
     )
     is_enabled = models.BooleanField(verbose_name='Enabled', default=True, null=False)
     inject_stubborn_headers = models.BooleanField(verbose_name='Inject Stubborn Headers', default=False)
@@ -421,7 +421,7 @@ class Team(BaseStubModel):
     name = models.CharField(verbose_name='Name', max_length=50, blank=False, null=False)
     slug = models.SlugField(verbose_name='Slug', allow_unicode=True, blank=False, null=False)
     owner = models.ForeignKey(
-        Member,
+        User,
         verbose_name='Team Owner',
         null=False,
         blank=False,
--- apps/services.py
+++ apps/services.py
@@ -6,6 +6,7 @@ from typing import Any, TypeVar, cast
 
 import requests
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
 from django.http import Http404
 from django.shortcuts import get_object_or_404
@@ -16,7 +17,7 @@ from rest_framework_xml.renderers import XMLRenderer
 
 from apps import enums, hooks, models
 from apps.enums import ResponseChoices
-from apps.models import Application, RequestLog, ResourceStub, ResponseStub, Member
+from apps.models import Application, RequestLog, ResourceStub, ResponseStub
 from apps.renderers import SimpleTextRenderer
 from apps.serializers import ApplicationSerializer
 from apps.utils import add_stubborn_headers, clean_headers, log_response
@@ -25,6 +26,7 @@ logger = logging.getLogger(__name__)
 
 StubbornSwitchableResource = TypeVar('StubbornSwitchableResource')
 
+user_model = get_user_model()
 
 def request_log_create(
     application: Application,
@@ -262,7 +264,7 @@ def turn_off_same_resource(resource: Application | ResourceStub) -> Application 
 
 
 def save_application_from_json_object(
-    jsonyfied_file_data: dict[str, Any], update: bool | None = False, user: Member | None = None
+    jsonyfied_file_data: dict[str, Any], update: bool | None = False, user: user_model | None = None
 ) -> Application:
     """Create or update an existing Application from the given file object.
 
@@ -288,7 +290,7 @@ def save_application_from_json_object(
         serialized_application = ApplicationSerializer(data=jsonyfied_file_data)
 
     if not user:
-        users_list = Member.objects.filter(is_staff=True, is_active=True).order_by('date_joined')
+        users_list = user_model.objects.filter(is_staff=True, is_active=True).order_by('date_joined')
         if users_list:
             user = users_list.first()
 
